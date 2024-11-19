29aeb2c23693365b34f83d72281d857bd5451f3b
Victor Rodriguez <victormanuelrodriguez0899@gmail.com>
2024-11-18 19:44:18 -0600

    fix:(challenge): resolve Module 2: JavaScript Refresher Challenges

--- JS-Algorithms/challenges.js
+++ JS-Algorithms/challenges.js
@@ -24,7 +24,7 @@ const readableTime = (seconds) => {
 
   let minutes = Math.floor ((seconds % 3600)/60); // to get the minutes calculated, i used the (% remainder) operator , bc its gave me the residue of the division, in this example the result remainder is 0.127222... but if the dividend (458)is smaller than the divisor (3600) the result is the divident itself. So in this case 458 is small than 3600 so the result is 458, so you take that number and divide by 60 bc you want to know the minutes, each minute has 60 s so thas why you divided by 60. resulting in 7.6333... but the floor method ignore the decimals and take only the 7. 
 
-  let seconds = seconds % 60;// to get the seconds, it works the same as the first part of the minutes calculation. takes the 458 s and use the operator (% remainder) and this got u a 7.38, 38 s is your SS bc we divided seconds by seconds so its not needed to do a conversion. 
+  let secs = seconds % 60;// to get the seconds, it works the same as the first part of the minutes calculation. takes the 458 s and use the operator (% remainder) and this got u a 7.38, 38 s is your SS bc we divided seconds by seconds so its not needed to do a conversion. 
 
   return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(secs).padStart(2, '0')}`; //Just concatenate the hours+minutes+secs and use the metohd .padStart to add the 0 at the begin to achieve the lengh you want .
 };
@@ -60,22 +60,18 @@ Invoking "circularArray(2)" should return "["Island", "Japan", "Israel", "German
 
 const COUNTRY_NAMES = ["Germany", "Norway", "Island", "Japan", "Israel"];
 
-//INDEX
-//Germany [0]
-//Norway [1]
-//Island [2]
-//Japan [3]
-//Israel [4]
-
 const circularArray = (index) => {
   if (index <0) {
     return "Index is not a positive number"
   }
   let arrayLenght = COUNTRY_NAMES.length; //i used the method .leght to calculate the total leght of the array, 5 elements.[Germany, Norway, Island,Japan,Israel]
 
-  let begin = COUNTRY_NAMES.slice(index % length); //to calculate the begin of the array i used the .slice method to determinate the begin, for example you type index=1 , this gonna return a 1%5 wich is equal to 1 .
+  let start = index % arrayLenght;
 
-  let end = COUNTRY_NAMES.slice(0, index % length); //to calculate the end of the array this .slice method bring back the 0 index until the calculated begin. 
+
+  let begin = COUNTRY_NAMES.slice(start); //to calculate the begin of the array i used the .slice method to determinate the begin, for example you type index=1 , this gonna return a 1%5 wich is equal to 1 .
+
+  let end = COUNTRY_NAMES.slice(0,start); //to calculate the end of the array this .slice method bring back the 0 index until the calculated begin. 
 
   return begin.concat(end); 
 
@@ -109,19 +105,13 @@ The last 3 digits for the sum of powers from 1 to 10 is "317"
 
 const ownPower = (number, lastDigits) => {
 
-  let sum = 0; //initialize the sum on 0 
-
-  for (let i = 1;i <= number; i++) { // i choose a "for loop" bc i read about the other ways to do it, instead of "while loop" or "recursion" this seems to be the best bc is most "efficient" just compare the number that the user wrhite and compares to the i value, if its lower continues adding numbers untill is higher.
+  let sum = BigInt(0); //initialize the sum on with bigint bc they need to be the same type
+  for (let i = 1; i <= number; i++) { // i choose a "for loop" bc i read about the other ways to do it, instead of "while loop" or "recursion" this seems to be the best bc is most "efficient" just compare the number that the user wrhite and compares to the i value, if its lower continues adding numbers untill is higher.
   
     sum  += BigInt(i) ** BigInt(i); // used += to reasignate the sum by itself and use big int bc the exponential numbers are big 
   }
-  
-
-
   const sumString = sum.toString(); //take the sum and convert it to a string, so i can use the method slice to take the last (n) digits 
   return sumString.slice(-lastDigits);
-
-
 };
 
 console.log(ownPower(10, 3)); 
